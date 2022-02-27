5215f660cea7b6e3ffeeaab4272d4b9b474c2e79
Windmill-City <1449182174@qq.com>
2022-02-27 12:38:54 +0800

    Fix format util

--- src/include/FormatUtil.hpp
+++ src/include/FormatUtil.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <memory>
+#include <stdarg.h>
 #include <string>
 
 std::string format(const char* pMessage, ...)
@@ -6,9 +8,12 @@ std::string format(const char* pMessage, ...)
     va_list args = NULL;
     va_start(args, pMessage);
 
+    char buf[256];
+    auto len = vsnprintf(buf, 256, pMessage, args);
+
     va_end(args);
 
-    auto result = std::string(pBufMsg);
+    auto result = std::string(buf, len);
 
     return result;
 }
@@ -18,7 +23,12 @@ std::wstring format(const wchar_t* pMessage, ...)
     va_list args = NULL;
     va_start(args, pMessage);
 
-    auto result = std::wstring(pBufMsg);
+    wchar_t buf[256];
+    auto    len = vswprintf(buf, 256, pMessage, args);
+
+    va_end(args);
+
+    auto result = std::wstring(buf, len);
 
     return result;
 }
